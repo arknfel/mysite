2668dfc38d3189aa0befa91618254222f50dec7f
arknfel <most.kaseb@gmail.com>
2022-08-02 03:54:50 +0200

    Room model

--- base/urls.py
+++ base/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import index
+from .views import index, room, rooms
 
 
 urlpatterns = [
     path('', index, name='base-index'),
+    path('room/<str:roomId>/', room, name='base.room'),
+    path('room/', rooms, name='base.rooms'),
 ]
--- base/views.py
+++ base/views.py
@@ -1,5 +1,24 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render
+from django.http import HttpResponseNotFound
 
-# Create your views here.
+from .models import Room
+
+
+
 def index(request):
-    return HttpResponse("HomePage")
+    return render(request, 'base/home.html')
+
+
+def room(request, roomId):
+    result = Room.objects.filter(id=roomId).first()
+    print(result)
+    
+    if not result:
+        return HttpResponseNotFound
+    return render(request, 'base/room.html', {'room': result})
+
+
+def rooms(request):
+    result = Room.objects.all().order_by('id')
+    print(result)
+    return render(request, 'base/rooms.html', {'rooms': result})
